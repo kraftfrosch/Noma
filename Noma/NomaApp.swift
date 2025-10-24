ead06f725d2fb56ff231af01f397a28b5677cefe
Joshua Kraft <joshua.kraft@cdtm.de>
2025-10-24 22:02:32 +0200

    Initial Commit

---
+++ Noma/NomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomaApp.swift
+//  Noma
+//
+//  Created by Joshua Kraft on 24.10.25.
+//
+
+import SwiftUI
+
+@main
+struct NomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NomaTests/NomaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NomaTests.swift
+//  NomaTests
+//
+//  Created by Joshua Kraft on 24.10.25.
+//
+
+import Testing
+@testable import Noma
+
+struct NomaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
